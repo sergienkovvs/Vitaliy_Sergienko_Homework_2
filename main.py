7fc2a6972e547ba9357375653907c0ba8707821c
sergienkovvs <110195868+sergienkovvs@users.noreply.github.com>
2022-09-03 20:24:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
-# Qsetion #1
-same_value_and_id_1 = 1
-same_value_and_id_2 = 1
-same_value_and_id_3 = 1
-is_value_eqv = same_value_and_id_1 == same_value_and_id_2 == same_value_and_id_3
-is_id_eqv = same_value_and_id_1 is same_value_and_id_2 is same_value_and_id_3
+value1 = 123
+value2 = 123
+value3 = 123
 
-print(id(same_value_and_id_1),id(same_value_and_id_2),id(same_value_and_id_3))
-print(is_value_eqv)
-print(is_id_eqv)
+print(id(value1))
+print(id(value2))
+print(id(value3))
 
-# Qestion #2
-changeble_1 = 255
-changeble_2 = 255.0
+key1 = list("pass")
+key2 = list("pass")
 
-print(changeble_1 == changeble_2)
-print(changeble_1 is changeble_2)
+print(id(key1))
+print(id(key2))
 
-# Qestion #3
-same_value_and_id_1 = 1
-same_value_and_id_2 = 1
-same_value_and_id_3 = 1
+value1 = [value1]
+value2 = [value2]
+value3 = [value3]
 
-changeble_1 = 255
-changeble_2 = 255.0
+print(id(value1))
+print(id(value2))
+print(id(value3))
 
-print(int(same_value_and_id_1) is float(same_value_and_id_2) is str(same_value_and_id_3))
-print(int(changeble_1) is int(changeble_2))
+key1 = bool(key1)
+key2 = bool(key2)
+
+print(id(key1))
+print(id(key2))
